9bbadb4a4629ab2c1518efd947a6dfd848d67ef9
Scott Griffin <scott@loggly.com>
2014-07-23 21:18:26 +0000

    Added flask security requirements and initial configs

--- law/web/__init__.py
+++ law/web/__init__.py
@@ -2,24 +2,41 @@
 " Copyright:    Loggly, Inc.
 " Author:       Scott Griffin
 " Email:        scott@loggly.com
-" Last Updated: 07/15/2014
+" Last Updated: 07/23/2014
 "
 " Flask interface for the web module
 "
 """
-import os
-
 from datetime            import datetime, date
-from flask               import Flask
+from flask               import Flask, render_template
 from flask.json          import JSONEncoder
 from law                 import config
-from law.web             import subscription
+from law.web             import views, subscription
+from law.util.adb        import Session, AccountState, Tier
+from law.util.lawdb      import db_url, db, security
+from flask.ext.login     import current_user, current_app, login_user
 
 app = Flask( config.get( 'webapp', 'name' ) )
 app.debug = config.getboolean( 'webapp', 'debug' )
 
+# Blueprints and top-level routes
+app.register_blueprint( views.blueprint )
+
 app.register_blueprint( subscription.views.blueprint, url_prefix = '/subscription' )
-app.register_blueprint( subscription.rest.blueprint, url_prefix = '/rest/subscription' )
+app.register_blueprint( subscription.rest.blueprint, url_prefix = '/apiv1/subscription' )
+
+# Config items
+app.config['SECRET_KEY']              = config.get( 'flask-security', 'secret_key' )
+app.config['SECURITY_PASSWORD_HASH']  = config.get( 'flask-security', 'password_hash' )
+app.config['SECURITY_PASSWORD_SALT']  = config.get( 'flask-security', 'password_salt' )
+app.config['SECURITY_TRACKABLE']      = True
+
+app.config['SQLALCHEMY_ECHO' ]        = config.getboolean( 'lawdb', 'debug' )
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+
+# LAWDB and flask-security app bindings
+db.init_app( app )
+security.init_app( app )
 
 class JSONLawDEncoder( JSONEncoder ):
     def default( self, obj ):
@@ -29,3 +46,23 @@ class JSONLawDEncoder( JSONEncoder ):
             super( JSONLawDEncoder, self ).default( obj )
 
 app.json_encoder = JSONLawDEncoder
+
+# Privileged blueprint registry
+
+def secure_blueprints( blueprints ):
+    """ This function forces all routes in the supplied
+    blueprint to force user authentication prior to serving
+    the URL
+    """
+    
+    def bp_login_required():
+        if not current_user.is_authenticated():
+            return current_app.login_manager.unauthorized()
+
+    for bp in blueprints:
+        app.before_request_funcs.setdefault( bp.name, [] ).append( bp_login_required )
+
+secure_blueprints([
+    subscription.views.blueprint,
+    subscription.rest.blueprint,
+])
