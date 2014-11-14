90bbfeed34fb03a1067837b1e7d8af52ed6d49be
Scott Griffin <scott@loggly.com>
2014-11-14 09:14:32 +0000

    Refactored to use a revealing object pattern

--- law/web/salesdash/static/js/charts.js
+++ law/web/salesdash/static/js/charts.js
@@ -5,81 +5,113 @@
 //
 //
 define( ['jquery', 'd3', 'nvd3'], function( $, d3, nv ) {
-    var barChart = function( selector, data, config ) {
-        return nv.addGraph(function() {
-            var utc_offset = new Date().getTimezoneOffset() * 60 * 1000 ;
-            var chart = nv.models.multiBarChart()
-                        .margin( {right:150} )
-                        .x( function(d) { return d[0] })
-                        .y( function(d) { return d[1] })
-                        .rightAlignYAxis(true) 
-                        .transitionDuration(150)
-                        .reduceXTicks(true)
-                        .rotateLabels(0)
-                        .stacked( true )
-                        .showControls(true)
-                        .tooltips(true)
-                        .groupSpacing(0.1) ;
-//                        .color( ['#236B9E', '#9E5623'] ) ;
-//                        .color( ['#E5975F', '#DA5FE4', '#69E45F', '#5FACE4'] );
+    var BarChart = function( selector, data, config ) {
 
-            if( config.timestamped ) {
-                chart.xAxis.tickFormat( function( d ) {
-                    return d3.time.format( '%Y-%m-%d' )(new Date(d + utc_offset ) ) ;
-                });
-            } 
+        var utc_offset = new Date().getTimezoneOffset() * 60 * 1000 ;
+        var _chart     = nv.models.multiBarChart()
+                           .margin( {right:150} )
+                           .x( function(d) { return d[0] })
+                           .y( function(d) { return d[1] })
+                           .rightAlignYAxis(true) 
+                           .transitionDuration(150)
+                           .reduceXTicks(true)
+                           .rotateLabels(0)
+                           .stacked( true )
+                           .showControls(true)
+                           .tooltips(true)
+                           .groupSpacing(0.1) ;
+//                           .color( ['#236B9E', '#9E5623'] ) ;
+//                           .color( ['#E5975F', '#DA5FE4', '#69E45F', '#5FACE4'] );
+        var _dataUrl   = config.dataUrl ;
+        var _d3Obj     = null;
+        var _bucketed  = 'quarter' ;
+        var _start     = null;
+        var _end       = null;
 
-            chart.yAxis.tickFormat( function( d ) {
-                return ' $' + d3.format( ',.2f' )( d ) ;
+        if( config.timestamped ) {
+            _chart.xAxis.tickFormat( function( d ) {
+                return d3.time.format( '%Y-%m-%d' )(new Date(d + utc_offset ) ) ;
+            });
+        } 
+
+        _chart.yAxis.tickFormat( function( d ) {
+            return ' $' + d3.format( ',.2f' )( d ) ;
+        });
+
+        // Set the data for the chart and render
+        _d3Obj = d3.select( selector )
+                   .datum( data )
+                   .call(_chart);
+        
+        // Bind the D3 data store to this chart object for easy updating
+//        _chart.datum = function( data ) {
+//            d3Obj.datum( data );
+//            return _chart
+//        }
+
+        nv.utils.windowResize(_chart.update);
+        nv.addGraph(function() { return _chart } ) ;
+
+        if( config.timeBuckets ) {
+            var buttons = $( '<div id="bucket-control" align="center"></div>' )
+
+            $( config.timeBuckets ).each( function( i, val ) {
+                var html ;
+                if( _bucketed === val ) { 
+                     html = '<input type="radio" data-bucket="' + val + '" id="' + val + '" name="project" checked="checked">' ;
+                } else {
+                     html = '<input type="radio" data-bucket="' + val + '" id="' + val + '" name="project">' ;
+                }
+                html += '<label for="' + val + '">' + val + '</label>' ;
+                $( buttons ).append( html );
             });
 
+            buttons.buttonset()
 
-            var d3Obj = d3.select( selector )
-              .datum( data )
-              .call(chart);
-            
-            // Bind the D3 data store to this chart object for easy updating
-            chart.datum = function( data ) {
-                d3Obj.datum( data );
-                return chart
-            }
+            // Bind the button selection
+            buttons.insertAfter( selector ) ;
+            $( buttons ).find( 'input' ).change( function() {
+                _bucketed = $(this).data( 'bucket' ) ;
+                updatePeriod( _start, _end, _bucketed ) ;
+            });
+        }
 
-            nv.utils.windowResize(chart.update);
+        if( config.dateSlider ) {
 
-            if( config.timeBuckets ) {
-                var buttons = createTimeBucketing( config.timeBuckets ) ;
-                buttons.insertAfter( selector ) ;
-                start = '2014-04-08' ;
-                end   = '2014-12-31' ;
-                $( buttons ).find( 'input' ).change( function() {
-                    updatePeriod( chart, config.dataUrl, start, end, $(this).data('bucket') ) ;
-                });
-            }
+            // Set initial values based on the slider
+            values = config.dateSlider.dateRangeSlider( 'values' ) ;
+            _start = values.min.toISOString().split( 'T' )[0];
+            _end   = values.max.toISOString().split( 'T' )[0];
 
-            if( config.dateSlider ) {
-                config.dateSlider.bind("valuesChanged", function(e, data){
-                    var start = data.values.min.toISOString().split( 'T' )[0];
-                    var end = data.values.max.toISOString().split( 'T' )[0];
-                    updatePeriod( chart, config.dataUrl, start, end, 'quarter' ) ;
-                });
-            }
+            config.dateSlider.bind("valuesChanged", function(e, data){
+                _start = data.values.min.toISOString().split( 'T' )[0];
+                _end = data.values.max.toISOString().split( 'T' )[0];
+                updatePeriod( _start, _end, _bucketed ) ;
+            });
+        }
 
-            return chart ;
-        });
-    };
+        var updatePeriod = function( start, end, bucketby ) {
+            params = $.param( {start:start, end:end, bucketed:bucketby} ) ;
+            var url = _dataUrl + '?' + params;
 
-    var createTimeBucketing = function( buckets ) {
-        var container = $( '<div id="bucket-control" align="center"></div>' )
-
-        $( buckets ).each( function( i, val ) {
-            $( container ).append( 
-                $( '<input type="radio" data-bucket="' + val + '" id="' + val + '" name="project">' +
-                   '<label for="' + val + '">' + val + '</label>' )
-            );
-        });
-
-        container.buttonset()
-        return container ;
+            d3.json( url, function( error, json ) {
+                if( error ) {
+                    return;
+                }
+                _d3Obj.datum(json.series) ;
+                _chart.update() ;
+//                _start = json.series[0][0] ;
+//                _end   = json.series[0][0] ;
+            });
+        };
+                
+   
+        return {
+            updatePeriod : updatePeriod,
+            start        : _start,
+            end          : _end,
+            bucketed     : _bucketed,
+        };
     }
 
     var urlParams = function() {
@@ -93,20 +125,8 @@ define( ['jquery', 'd3', 'nvd3'], function( $, d3, nv ) {
         return params
     }
 
-    var updatePeriod = function( chart, dataUrl, start, end, bucketby ) {
-        params = $.param( {start:start, end:end, bucketed:bucketby} ) ;
-        var url = dataUrl + '?' + params;
-
-        d3.json( url, function( error, json ) {
-            if( error ) {
-                return;
-            }
-            chart.datum(json.series).update() ;
-        });
-    };
 
     return {
-        barChart: barChart,
-        updatePeriod: updatePeriod
+        BarChart: BarChart,
     }
 });
