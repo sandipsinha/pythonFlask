2356d920e76d4d066f4af1a4c045d78e85802e6d
Scott Griffin <scott@loggly.com>
2014-11-19 02:05:56 +0000

    Locked queries to only query Gen2 data.  Fixed timing on owners.  Added query_product_state

--- law/util/queries.py
+++ law/util/queries.py
@@ -9,15 +9,16 @@
 from collections        import defaultdict
 from operator           import attrgetter
 
-from sqlalchemy         import and_, or_
+from sqlalchemy         import and_, or_, not_, func
 from law.util.timeutil  import Timebucket
-from law.util.adb       import session_context, AccountState, Owners
+from law.util.adb       import session_context, AccountState, Owners, Tier
 
 def state_query( s, states, start, end ):
     # operator | or's the states together ( | is overloaded in SQLAlchemy query construction)
     q = s.query( AccountState ) \
          .filter( AccountState.updated >= start ) \
-         .filter( AccountState.updated <= end ) \
+         .filter( AccountState.updated < end ) \
+         .filter( AccountState.tPlan_id < 100 ) \
          .filter( reduce( lambda query, func: query | func, [ AccountState.state.like( state ) for state in states] ) ) \
 
     return q
@@ -25,6 +26,15 @@ def state_query( s, states, start, end ):
 def query_state( states, start, end ):
     with session_context() as s:
         subs = state_query( s, states, start, end ).all()
+        s.expunge_all()
+    return subs
+
+def query_product_state( states, products, start, end ):
+    with session_context() as s:
+        q = state_query( s, states, start, end )
+        q = q.join( Tier, aliased=True )\
+              .filter( or_(( Tier.name.__eq__( product ) for product in products )) )
+        subs = q.all()
         s.expunge_all()
     return subs
 
@@ -39,10 +49,13 @@ class QueryOwners(object):
     def _query_owners( self, states, start, end, owners=None):
         with session_context() as s:
             subq = state_query( s, states, start, end ).subquery()
+#            aliased_subq = aliased( AccountState, subq )
+#            q = s.query( Owners, aliased_subq )\
             q = s.query( Owners, subq )\
                 .join( subq, and_( Owners.acct_id == subq.c.acct_id,
-                                    subq.c.updated >= Owners.start_date,
-                                    subq.c.updated <  Owners.end_date ))
+                                    Owners.subdomain == subq.c.subdomain,
+                                    func.DATE( subq.c.updated ) >= Owners.start_date,
+                                    func.DATE( subq.c.updated ) <=  Owners.end_date ))
 
             # If we're only querying for specific users then apply the or'd constraints
             if owners != None:
