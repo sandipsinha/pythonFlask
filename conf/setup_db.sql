5a3a7963f37316ad329706a42fe9c240ed454509
Scott Griffin <scott@loggly.com>
2015-01-09 07:09:38 +0000

    Added adb test databases setup for testing purposes

--- conf/setup_db.sql
+++ conf/setup_db.sql
@@ -1,6 +1,12 @@
-CREATE DATABASE law_dev;
+CREATE DATABASE IF NOT EXISTS law_dev;
 CREATE USER 'lawdev'@'%' IDENTIFIED BY 'lawdev';
 GRANT ALL on law_dev.* TO 'lawdev'@'%';
 
+CREATE DATABASE IF NOT EXISTS law_test ;
 CREATE USER 'lawtest'@'%' IDENTIFIED BY 'lawtest';
 GRANT ALL on law_test.* TO 'lawtest'@'%';
+
+CREATE DATABASE IF NOT EXISTS law_adb_test ;
+GRANT ALL on law_adb_test.* TO 'lawtest'@'%';
+
+SELECT 'Setup DB: SUCCESS' AS '' ;
