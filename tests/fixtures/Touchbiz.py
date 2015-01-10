bd7f27d42b60816d5b7afbd67808d89c12f2bee7
Scott Griffin <scott@loggly.com>
2015-01-10 19:22:24 +0000

    Adjusted timings

--- tests/fixtures/Touchbiz.py
+++ tests/fixtures/Touchbiz.py
@@ -12,7 +12,19 @@ MODEL = Touchbiz
 FIXTURES = [
 {
     'acct_id'       : 1000,
-    'sales_rep_id'  : 1,
+    'sales_rep_id'  : 2,
+    'created'       : datetime( 2014, 5, 9 ),
+    'modified'      : datetime( 2014, 5, 9 ),
+    'stage_id'      : 2,
+    'tier'          : 'Development',
+    'retention'     : 7,
+    'volume'        : 100000000000,
+    'sub_rate'      : 49,
+    'billing_period': 'monthly',
+},
+{
+    'acct_id'       : 1001,
+    'sales_rep_id'  : 2,
     'created'       : datetime( 2014, 5, 7 ),
     'modified'      : datetime( 2014, 5, 7 ),
     'stage_id'      : 2,
