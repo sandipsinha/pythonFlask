94cd7ab8ca738c5321b88e687ccceab9f1f326ea
Scott Griffin <scott@loggly.com>
2015-02-13 08:37:49 +0000

    Added Owners fixture

---
+++ tests/fixtures/Owners.py
@@ -0,0 +1,29 @@
+"""
+" Copyright:    Loggly, Inc.
+" Author:       Scott Griffin
+" Email:        scott@loggly.com
+"
+"""
+from datetime import datetime
+
+from law.util.adb import Owners
+
+MODEL = Owners
+FIXTURES = [
+{
+    'acct_id'    : 1000,
+    'subdomain'  : 'tbmigration',
+    'owner'      : 'Angela Eichner',
+    'start_date' : datetime( 2014, 4, 8 ),
+    'end_date'   : datetime( 2014, 4, 9 ),
+    'executive'  : 0,
+},
+{
+    'acct_id'    : 1000,
+    'subdomain'  : 'tbmigration',
+    'owner'      : 'Cristina Quintero',
+    'start_date' : datetime( 2014, 4, 10 ),
+    'end_date'   : datetime( 9999, 12, 31 ),
+    'executive'  : 0,
+},
+]
