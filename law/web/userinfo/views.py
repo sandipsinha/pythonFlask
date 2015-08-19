b80740b7491ec2afe21cb94a07a4c70e59191da2
Sandip Sinha <amsandip@yahoo.com>
2015-08-19 21:13:10 +0000

    Module for tracking User Data within SFDC

---
+++ law/web/userinfo/views.py
@@ -0,0 +1,48 @@
+
+"""
+" Copyright:    Loggly, Inc.
+" Author:       Sandip Sinha
+" Email:        ssinha@loggly.com
+"
+"""
+from flask                import Blueprint, render_template, request, flash,redirect, json
+from law.util.queries    import query_user_state
+
+
+
+blueprint = Blueprint( 'userinfo', __name__,
+                        template_folder = 'templates',
+                        static_folder   = 'static' )
+
+
+
+@blueprint.route( '/', methods=['GET'])
+def display_user_info():
+    subd= request.args['subdomain']
+    return render_template('userinfo/displayuser.html', grid2=subd)
+
+@blueprint.route( '/logindata', methods=['GET', 'POST'] )
+def get_user_data():
+    subd = request.form.get('subdomain',' ')
+
+    users = query_user_state(subd)
+    recid = 0
+    userlist = []
+    userqueue = {}
+    for row in users:
+        userdat = {}
+        recid += 1
+        userdat['recid'] = recid
+        userdat['userid'] = int(row.Users.user_id)
+        userdat['username'] = row.Users.username
+        userdat['email'] = row.Users.email
+        userdat['Number_Of_Logins'] = row.Number_Of_Logins
+        userdat['Last_Login'] = str(row.Last_Login.strftime('%Y/%m/%d'))
+        userlist.append(userdat)
+    if recid > 0:
+        userqueue['status'] = 'success'
+        userqueue['total'] = recid
+        userqueue['records'] = userlist
+    return json.dumps(userqueue)
+
+
---
+++ tests/fixtures/UserTracking.py
@@ -0,0 +1,84 @@
+"""
+" Copyright:    Loggly, Inc.
+" Author:       Scott Griffin
+" Email:        scott@loggly.com
+"
+"""
+from datetime import datetime
+
+from law.util.queries import UserTracking
+
+MODEL = UserTracking
+FIXTURES = [
+{
+    'user_id'    : 2001,
+    'username'   : 'iamactor',
+    'session_id' : 'User11',
+    'login'      :  datetime( 2014, 1, 8 ),
+    'email'      : 'tom.cruise@loggly.com',
+},
+{
+    'user_id'    : 2001,
+    'username'   : 'iamactor',
+    'session_id' : 'User12',
+    'login'      :  datetime( 2014, 4, 5 ),
+    'email'      : 'tom.cruise@loggly.com',
+},
+{
+    'user_id'    : 2001,
+    'username'   : 'iamactor',
+    'session_id' : 'User13',
+    'login'      :  datetime( 2014, 7, 9 ),
+    'email'      : 'tom.cruise@loggly.com',
+},
+{
+    'user_id'    : 2001,
+    'username'   : 'iamactor',
+    'session_id' : 'User14',
+    'login'      :  datetime( 2015, 2, 9 ),
+    'email'      : 'tom.cruise@loggly.com',
+},
+{
+    'user_id'    : 2002,
+    'username'   : 'iamsports',
+    'session_id' : 'User21',
+    'login'      :  datetime( 2014, 3, 9 ),
+    'email'      : 'carl.lewis@loggly.com',
+},
+{
+    'user_id'    : 2002,
+    'username'   : 'iamsports',
+     'session_id' : 'User22',
+    'login'      :  datetime( 2013, 7, 9 ),
+    'email'      : 'carl.lewis@loggly.com',
+},
+{
+    'user_id'    : 2003,
+    'username'   : 'iamscientist',
+     'session_id' : 'User31',
+    'login'      :  datetime( 2014, 7, 9 ),
+    'email'      : 'albert.einstein@loggly.com',
+},
+{
+    'user_id'    : 2003,
+    'username'   : 'iamscientist',
+     'session_id' : 'User32',
+    'login'      :  datetime( 2015, 7, 9 ),
+    'email'      : 'albert.einstein@loggly.com',
+},
+{
+    'user_id'    : 2003,
+    'username'   : 'iamscientist',
+     'session_id' : 'User33',
+    'login'      :  datetime( 2014, 8, 9 ),
+    'email'      : 'albert.einstein@loggly.com',
+},
+{
+    'user_id'    : 2005,
+    'username'   : 'iamscientist1',
+    'session_id' : 'User33',
+    'login'      :  datetime( 2012, 6, 9 ),
+    'email'      : 'acharya.bose@loggly.com',
+}
+
+]
