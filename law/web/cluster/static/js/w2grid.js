a9e27bddde08306d94e80ee9194bd4251838b67e
Sandip Sinha <amsandip@yahoo.com>
2015-12-06 19:09:30 -0800

    Updated JS grid to allow sorting without refresh

--- law/web/cluster/static/js/w2grid.js
+++ law/web/cluster/static/js/w2grid.js
@@ -12,7 +12,6 @@ $(window).load(function () {
     $('#grid').w2grid({
         name: 'grid',
         header: 'List of Subdomains',
-        url: '/apiv1/cluster/subdgrid/',
         show : {
             header         : false,
             toolbar        : true,
@@ -42,7 +41,7 @@ $(window).load(function () {
 
     });
     w2ui['grid'].postData  = PostData;
-
+    w2ui['grid'].load('/apiv1/cluster/subdgrid/');
 
     $('#clstrdata').change(function(event){
       event.preventDefault();
