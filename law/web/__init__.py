b0976835acead22997bee329d3b058bea49d5510
Sandip Sinha <amsandip@yahoo.com>
2016-04-06 22:54:57 -0700

    Added missing js script into editable touchbiz frame routine

--- law/web/__init__.py
+++ law/web/__init__.py
@@ -14,7 +14,7 @@ from flask               import Flask, url_for
 from flask.json          import JSONEncoder
 from law                 import config
 from law.web             import views, subscription, volumes, salesdash, touchbiz, salesorder, userinfo, tracer, \
-                         cluster, analyticsdash
+                         cluster
 from law.util.adb        import Session, AccountState, Tier
 from law.util.lawdb      import db_url, db, security
 from flask.ext.login     import current_user, current_app, login_user
@@ -35,7 +35,7 @@ app.register_blueprint( touchbiz.views.blueprint, url_prefix = '/touchbiz' )
 app.register_blueprint( salesorder.views.blueprint, url_prefix = '/salesorder' )
 app.register_blueprint( tracer.views.blueprint, url_prefix = '/tracer' )
 app.register_blueprint( cluster.views.blueprint, url_prefix = '/cluster' )
-app.register_blueprint( analyticsdash.views.blueprint, url_prefix = '/bidash' )
+
 
 # API routes
 app.register_blueprint( subscription.rest.blueprint, url_prefix = '/apiv1/subscription' )
@@ -44,7 +44,7 @@ app.register_blueprint( touchbiz.rest2.blueprint, url_prefix = '/apiv2/touchbiz'
 app.register_blueprint( userinfo.rest.blueprint, url_prefix = '/apiv1/clientinfo' )
 app.register_blueprint( tracer.rest.blueprint, url_prefix = '/apiv1/tracer' )
 app.register_blueprint( cluster.rest.blueprint, url_prefix = '/apiv1/cluster' )
-app.register_blueprint( analyticsdash.rest.blueprint, url_prefix = '/apiv1/bidash' )
+
 
 # Config items
 app.config['SECRET_KEY']              = config.get( 'flask-security', 'secret_key' )
